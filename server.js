cd63f530c01c62f07f12235d436b6c8856739e63
Jim <b97201019@gmail.com>
2020-01-28 14:58:23 +0900

    print out the port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const port = process.env.PORT || 3000
+
 app.use(express.static(path.join(__dirname, 'build')))
 // app.get('/', function(req, res) {
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'))
@@ -8,4 +10,6 @@ app.use(express.static(path.join(__dirname, 'build')))
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
-app.listen(process.env.PORT || 3000)
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`)
+})
