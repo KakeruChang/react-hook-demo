4a74837d0fe3350916ae5078156011db8872d450
Jim <b97201019@gmail.com>
2020-02-26 14:28:50 +0900

    fix the font size of main button for iphone 5

--- src/components/HomePage/MainButton.js
+++ src/components/HomePage/MainButton.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import MainButtonImg from '../../assets/order_top_btn_form.png'
+import useWindowSize from '../../hooks/useWindowSize'
 
 const MainButton = () => {
+  const { width } = useWindowSize()
+
   return (
     <div className='bg-primary text-light text-center py-4'>
       <div className='container'>
-        <h1>
+        <h1 className={width <= 576 ? 'h3' : ''}>
           LinksMateで
           <span className='text-warning'>もっとゲームを楽しもう！</span>
         </h1>
